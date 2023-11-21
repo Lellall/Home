fc2138847940b2e038c7378e9a336772601ee611
Yahya <ahmad.yahya@flexisaf.com>
2023-11-21 03:28:50 +0100

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -183,7 +183,7 @@ const Contained = styled.div`
   position: relative;
   @media (max-width: 500px) {
     padding-top: 150px;
-    min-height: 40dvh !important;
+    min-height: 45dvh !important;
     background: url(${Mobile});
     background-repeat: no-repeat;
     overflow-x: hidden;
