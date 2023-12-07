f9e9d456c5ea9af839701112943af7abce186662
ChiamakaUI <55190142+ChiamakaUI@users.noreply.github.com>
2023-12-07 19:21:17 +0100

    navbar and footer

--- src/app/app-layout.jsx
+++ src/app/app-layout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
+import { Footer, Navbar } from "../features/ui";
 
 const AppLayout = () => {
   return (
-    <div>
-      <p>hey there</p>
+    <>
+      <Navbar />
       <Outlet />
-    </div>
+      <Footer />
+    </>
   );
 };
 
