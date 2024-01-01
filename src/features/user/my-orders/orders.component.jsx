b322c3ae277c08e38de25d7eab034f92b4a2f03d
ChiamakaUI <55190142+ChiamakaUI@users.noreply.github.com>
2024-01-01 12:59:15 +0100

    order page conversion

--- src/features/user/my-orders/orders.component.jsx
+++ src/features/user/my-orders/orders.component.jsx
@@ -1,8 +1,50 @@
+import { useState } from "react";
 
+import {
+  HeadingContainer,
+  HeadingText,
+  TabContainer,
+  StyledButton,
+  EmptyState,
+  MainContainer,
+} from "./orders.styles";
 const Main = () => {
-  return (
-    <div>Main</div>
-  )
-}
+  const [currentTab, setCurrentTab] = useState(1);
 
-export default Main
+  const options = ["Pending (2)", "Completed (0)"];
+
+  const data = false;
+  return (
+    <MainContainer>
+      <HeadingContainer>
+        <HeadingText>Orders (2)</HeadingText>
+      </HeadingContainer>
+      <TabContainer>
+        {options.map((d, i) => (
+          <StyledButton
+            key={i}
+            onClick={() => setCurrentTab(i + 1)}
+            active={currentTab === i + 1}
+          >
+            {d}
+          </StyledButton>
+        ))}
+      </TabContainer>
+      <>
+        {data ? (
+          <div>Hey there</div>
+        ) : (
+          <EmptyState>
+            <img src="/assets/user-order.svg" alt="favorites" />
+            <div className="text-container">
+              <p className="bold">Nothing Yet!</p>
+              <p>All your Closed Orders will be saved here.</p>
+            </div>
+          </EmptyState>
+        )}
+      </>
+    </MainContainer>
+  );
+};
+
+export default Main;
