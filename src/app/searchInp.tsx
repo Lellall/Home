665f8c162e2e48bddf7233203e566d698b3fa68a
Yahya <yahay.me@gmail.com>
2024-03-26 01:35:35 +0100

    update search

--- src/app/searchInp.tsx
+++ src/app/searchInp.tsx
@@ -24,7 +24,7 @@ const SearchResults = styled.div`
   border-top: none;
   border-radius: 0 0 4px 4px;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-  z-index: 100;
+  z-index: 300000;
 `;
 
 const SearchResultItem = styled.div`
