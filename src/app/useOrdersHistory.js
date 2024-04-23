1ff3a1550c6ed99af2537ce0ad50531d9bf192ba
MujtabaAdamu <mujtaba.adamu@flexisaf.com>
2024-04-24 00:31:37 +0100

    add order history state and store

---
+++ src/app/useOrdersHistory.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+import { create } from 'zustand';
+import { BaseUrl } from '../utils/config';
+import localforage from 'localforage';
+
+//expected status parameters: PENDING, ACCEPTED, ON_GOING, COMPLETED, CANCELED
+const useOrdersHistory = create((set) => ({
+  ordersHistory: [],
+
+  setOrdersHistory: (val) => set({ val }),
+
+  fetchOrdersHistory: async (page, status) => {
+    const token = await localforage.getItem('accessToken');
+    if (token !== null) {
+      try {
+        const response = await axios.get(
+          `${BaseUrl}/orders/consumer/history?page=${page}&size=10&status=${status}`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        const newData = response.data.data;
+        console.log(
+          'localforage.getItem::::',
+          localforage.getItem('accessToken')
+        );
+        set(() => ({ ordersHistory: newData }));
+      } catch (error) {
+        console.error('Error fetching orders:', error);
+      }
+    }
+  },
+}));
+
+export default useOrdersHistory;
