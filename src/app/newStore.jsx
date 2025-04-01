23e8daea248b5b54165338148fbc2789934d6a34
Yahya <yahay.me@gmail.com>
2025-04-01 06:25:39 +0100

    temp closed shop

--- src/app/newStore.jsx
+++ src/app/newStore.jsx
@@ -327,9 +327,16 @@ const NewStore = () => {
       {/* <AlertCards type="danger">
       Payment System Maintenance Notice: Our payment system is currently undergoing maintenance for improved performance and security. We apologize for any inconvenience. Please bear with us as we work to resolve this issue. Thank you for your understanding.
       </AlertCards> */}
-      {/* <AlertCards type="danger">
-        Dear Customers,
-        We will be temporarily closed for the holiday season from now until January 5, 2025. We wish everyone a joyful holiday and a prosperous New Year!      </AlertCards> */}
+     <AlertCards type="danger">
+  <strong>Eid Announcement: 🌙✨</strong>
+  <br />
+  We will be temporarily closed in celebration of <strong>Eid al-Fitr</strong> from now until <strong>02/04/2025</strong>. 🎉  
+  <br /><br />
+  Wishing everyone a joyous Eid filled with happiness, peace, and prosperity! 🌟  
+  <br /><br />
+  <strong>Eid Mubarak! 🕌🥳</strong>
+</AlertCards>
+
       <Container>
         <Categories>
           <CategoriesList isSelectCategory={isSelectCategory} setIsSelectCategory={setIsSelectCategory} />
